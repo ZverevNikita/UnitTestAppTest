fa29feb5104808dc726141250f3ebde4ef83554b
ZverevNikita <zverev.nik95@gmail.com>
2022-10-27 09:51:17 +0300

    Добавьте файлы проекта.

---
+++ UnitTestApp.Models/UnitTestApp.Models.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace UnitTestApp.Models
+{
+    public interface IRepository
+    {
+        IEnumerable<User> GetAll();
+        User Get(int id);
+        void Create(User user);
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ UnitTestApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestApp.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15906f30-0954-4a22-99b5-28c939ddcf94")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
